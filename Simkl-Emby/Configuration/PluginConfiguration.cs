fdbfb9e01346d351cf84511a8a06d7464f91972b
David Davó <david@ddavo.me>
2018-06-30 02:14:16 +0200

    Started configuration

---
+++ Simkl-Emby/Configuration/PluginConfiguration.cs
@@ -0,0 +1,32 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Text;
+using MediaBrowser.Model.Plugins;
+
+namespace Simkl.Configuration
+{
+    /// <summary>
+    /// Class needed to create a Plugin and configurate it
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration // From MediaBrowser.Model.Plugins
+    {
+        // id's based on simkl's kodi add-on
+        // https://github.com/SIMKL/script.simkl/blob/master/resources/settings.xml
+
+        public bool autoscrobble { get; set; }  // Default true, enable false
+        public int scr_pct { get; set; }    // Scrobbling percentage
+        public int scr_w_pct { get; set; }  // Scrobbling "now watching" percentage (only for movies?)
+        public bool bubble { get; set; }     // Show bubble while scrobbling
+        public int min_length { get; set; } // Minimum length for scrobbling
+
+        public PluginConfiguration()    // Default values
+        {
+            this.autoscrobble = true;
+            this.scr_pct = 70;
+            this.scr_w_pct = 5;
+            this.bubble = true;
+            this.min_length = 5;
+        }
+
+    }
+}
--- Simkl-Emby/Plugin.cs
+++ Simkl-Emby/Plugin.cs
@@ -1,8 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+// using System.IO;    // IPluginConfigurationPage
+// using System.Reflection;    // IPluginConfigurationPage
+
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Model.Plugins;
+
+using Simkl.Configuration;
 
 namespace Simkl
 {
-    public class Plugin
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
+        // public override string Name { get { return "Simkl TV Tracker"; } }
+        // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-operator#expression-body-definition
+        public override string Name => "Simkl TV Tracker";
+        public override Guid Id => new Guid("2ecd91d5-b14b-4b92-8eb9-52c098edfc87");
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer) { }
+
+        /* IHasWebPages */
+
+        public IEnumerable<PluginPageInfo> GetPages() => new[]   // As we use new T inside, we can make it an implicitly typed array
+            {
+                new PluginPageInfo
+                {
+                    Name = "Simkl",
+                    EmbeddedResourcePath = "Simkl-Emby.Configuration.configPage.html"
+                }
+            };
+
+        /* IPluginConfigurationPage */
+
+        // string Name implemented w/ BasePlugin implementation
+
+        /*
+
+        public ConfigurationPageType ConfigurationPageType => ConfigurationPageType.PluginConfiguration;
+        public IPlugin Plugin => new IPlugin;
+
+        public Stream GetHtmlStream()
+        {
+            return Assembly.Load(new AssemblyName()).GetManifestResourceStream("Simkl.Configuration.configPage.html");
+        }
+        */
     }
 }

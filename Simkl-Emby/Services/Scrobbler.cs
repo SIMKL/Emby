bf09375a0e8e3a10a41941315003c5116cbef92f
David Davó <david@ddavo.me>
2019-07-09 18:39:52 +0200

    Working with movies!

--- Simkl-Emby/Services/Scrobbler.cs
+++ Simkl-Emby/Services/Scrobbler.cs
@@ -6,6 +6,8 @@ using Simkl.Api;
 
 using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Controller.Session;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Model.Logging;
 using MediaBrowser.Model.Serialization;
 using MediaBrowser.Common.Net;
@@ -20,8 +22,7 @@ namespace Simkl.Services
         private readonly ILogger _logger;
         private readonly IJsonSerializer _json;
         private SimklApi _api;
-        private DateTime nextScrobble;
-        private string lastScrobbled;   // Library ID of last scrobbled item
+        private Dictionary<string, string> lastScrobbled;   // Library ID of last scrobbled item
 
         // public static Scrobbler Instance; Instance = this
         public Scrobbler(IJsonSerializer json, ISessionManager sessionManager, ILogManager logManager, IHttpClient httpClient)
@@ -30,57 +31,54 @@ namespace Simkl.Services
             _sessionManager = sessionManager;
             _logger = logManager.GetLogger("Simkl Scrobbler");
             _api = new SimklApi(json, _logger, httpClient);
-            nextScrobble = DateTime.Now;
+            lastScrobbled = new Dictionary<string,string>();
         }
 
         public void Run()
         {
             _sessionManager.PlaybackProgress += embyPlaybackProgress;
-            _sessionManager.PlaybackStart += embyPlaybackStart;
-            // _sessionManager.PlaybackStop
         }
 
         public void Dispose()
         {
             _sessionManager.PlaybackProgress -= embyPlaybackProgress;
-            _sessionManager.PlaybackStart -= embyPlaybackStart;
         }
 
-        private bool canBeScrobbled(long? playBackPositionTicks, long? runTimeTicks, int min_length, int scr_pct)
-        {
-            // Note: 1 tick = 0.1 ms, 1 min = 60 * 1000 * 10 tick
-            float percentageWatched = (float)playBackPositionTicks / (float)(runTimeTicks) * 100;
-            bool greaterThanMinLength = runTimeTicks > 60 * 10000 * min_length;
-            return percentageWatched > scr_pct && greaterThanMinLength && DateTime.Now > nextScrobble;
+        public static bool canBeScrobbled(UserConfig config, SessionInfo session) {
+            float percentageWatched = (float)(session.PlayState.PositionTicks) / (float)(session.NowPlayingItem.RunTimeTicks) * 100f;
+
+            // If percentage watched is below minimum, can't scrobble
+            if (percentageWatched < config.scr_pct) return false;
+            // If it's below minimum length, can't scrobble
+            if (session.NowPlayingItem.RunTimeTicks < 60 * 10000 * config.min_length) return false;
+
+            BaseItem item = session.FullNowPlayingItem;
+            if (item is Movie) return true; // TODO: Working only with movies
+
+            return false;
         }
         
         private void embyPlaybackProgress(object sessions, PlaybackProgressEventArgs e)
         {
-            // _logger.Debug(_json.SerializeToString(sessions));
-            // _logger.Debug(_json.SerializeToString(e));
-            bool v = lastScrobbled != e.MediaSourceId;
-            _logger.Debug("Current time: " + DateTime.Now + ", next scrobble: " + nextScrobble + ", notScrobbled: " + v);
-            _logger.Debug("PlaybackProgressEventArgs: " + _json.SerializeToString(e));
-            _logger.Debug(e.Session.UserId.ToString());
             UserConfig userConfig = Plugin.Instance.PluginConfiguration.getByGuid(e.Session.UserId);
             if (userConfig.userToken == "")
             {
-                _logger.Info("Can't scrobble: User " + e.Session.UserName + " not logged in");
-                v = false;
+                _logger.Error("Can't scrobble: User " + e.Session.UserName + " not logged in");
+                return;
             }
-            if (v && canBeScrobbled(e.PlaybackPositionTicks, e.MediaInfo.RunTimeTicks, userConfig.min_length, userConfig.scr_pct))
-            {
-                nextScrobble = DateTime.Now.AddSeconds(userConfig.scrobbleTimeout);
-                lastScrobbled = e.MediaSourceId;
-                _api.markAsWatched(e.MediaInfo, userConfig.userToken);
-            }
-        }
 
-        private void embyPlaybackStart(object obj, PlaybackProgressEventArgs e)
-        {
-            _logger.Debug("EMBY PLAYBACK STARTED");
-            _logger.Debug("Obj: " + _json.SerializeToString(obj));
-            _logger.Debug("Event: " + _json.SerializeToString(e));
+            if (canBeScrobbled(userConfig, e.Session))
+            {
+                string uid = e.Session.UserId, npid = e.Session.NowPlayingItem.Id;
+
+                if (!lastScrobbled.ContainsKey(uid) || lastScrobbled[uid] != npid) {
+                    _logger.Info("Trying to scrobble {0} for {1}", e.Session.NowPlayingItem.Name, e.Session.UserName);
+                    _api.markAsWatched(e.MediaInfo, userConfig.userToken);
+                    lastScrobbled[e.Session.UserId] = e.Session.NowPlayingItem.Id;
+                } else {
+                    _logger.Debug("Alredy scrobbled {0} for {1}", e.Session.NowPlayingItem.Name, e.Session.UserName);
+                }
+            }
         }
 
     }

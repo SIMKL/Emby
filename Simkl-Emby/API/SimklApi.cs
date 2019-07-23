17c45273cfd0ba448bde1441acd699ce3d870022
David Davó <david@ddavo.me>
2019-07-23 13:41:24 +0200

    Deleted apiary

--- Simkl-Emby/API/SimklApi.cs
+++ Simkl-Emby/API/SimklApi.cs
@@ -22,8 +22,8 @@ namespace Simkl.Api
         private readonly IHttpClient _httpClient;
 
         /* BASIC API THINGS */
-        // public const string BASE_URL = @"https://api.simkl.com";
-        public const string BASE_URL = @"http://private-9c39b-simkl.apiary-proxy.com";
+        public const string BASE_URL = @"https://api.simkl.com";
+        // public const string BASE_URL = @"http://private-9c39b-simkl.apiary-proxy.com";
 
         public const string REDIRECT_URI = @"https://ddavo.me/redirected?from=EmbySimkl";
         public const string APIKEY = @"27dd5d6adc24aa1ad9f95ef913244cbaf6df5696036af577ed41670473dc97d0";
